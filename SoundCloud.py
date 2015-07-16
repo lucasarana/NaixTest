ab9d153ac12390d84cf9e01e76203401e1fc12c3
lucasarana <arana_lucas@hotmail.com>
2015-07-16 09:44:04 -0300

    SoundCloud no es nada ahora.

--- SoundCloud.py
+++ SoundCloud.py
@@ -5,14 +5,18 @@ import soundcloud
 
 class SoundCloud():
     pass
-    # client = soundcloud.Client(
-    #     client_id = 'efb4da00b3d22f63a53c8f0b9b96c553',
-    #     client_secret = '6b8bbb0743b912695afd94fe0187c7b7',
-    #     redirect_uri='http://127.0.0.1:5000/'
-    # )
-    #
-    # redirect(client.authorize_url())
-    #
-    # access_token, expires, scope, refresh_token = client.exchange_token(
-    #     code=request.args.get('code'))
-    # render_text("Hi There, %s" % client.get('/me').username)
+
+    @staticmethod
+    def hello(handler):
+
+    client = soundcloud.Client(
+        client_id = 'efb4da00b3d22f63a53c8f0b9b96c553',
+        client_secret = '6b8bbb0743b912695afd94fe0187c7b7',
+        redirect_uri='http://localhost:2020/petitionresolved/'
+    )
+
+    handler.redirect(client.authorize_url())
+
+    access_token, expires, scope, refresh_token = client.exchange_token(
+        code=handler.request.args.get('code'))
+    render_text("Hi There, %s" % client.get('/me').username)
