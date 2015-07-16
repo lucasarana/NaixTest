092c2bfb2206e1f1106edd2b3bbbcf00071c663c
lucasarana <arana_lucas@hotmail.com>
2015-07-16 15:03:47 -0300

    Creo single DB (encargada de conectarse con MYSQL)

---
+++ SingleDB.py
@@ -0,0 +1,28 @@
+__author__ = 'lucas.arana'
+
+# -*- coding: utf-8 -*-
+import config
+import mysql.connector
+from mysql.connector import errorcode
+
+class SingleDB:
+    """Class to open, report or close the MYSQL connection"""
+
+    @staticmethod
+    def open_data_base():
+        """Opens data base"""
+        cnx = mysql.connector.connect(**config.db_params)
+
+        try:
+            cnx = mysql.connector.connect()
+        except mysql.connector.Error as err:
+          if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with your user name or password")
+          elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+          else:
+            print(err)
+        else:
+          cnx.close()
+
+        return cnx
