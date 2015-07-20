e203a165342b5b153747938e2d949a0067b4698b
lucasarana <arana_lucas@hotmail.com>
2015-07-20 15:25:30 -0300

    Modifico Clients (hay que ordenarlo)

--- Clients.py
+++ Clients.py
@@ -5,12 +5,17 @@ import soundcloud
 class Clients:
     """Class containing all necessary information from the Clients"""
 
-    def SoundCloudClient(self):
-        """Sound Clods detailed Client"""
+    def SoundCloudClient(self, access_token=[]):
+        """Sound Clouds detailed Client"""
+        if access_token:
+            sound_cloud_client = soundcloud.Client(
+                access_token=access_token
+                )
+        else:
+            sound_cloud_client = soundcloud.Client(
+                client_id = 'efb4da00b3d22f63a53c8f0b9b96c553',
+                client_secret = '6b8bbb0743b912695afd94fe0187c7b7',
+                redirect_uri='http://localhost:2020/petitionresolved'
+                )
 
-        soundCloudClient = soundcloud.Client(
-        client_id = 'efb4da00b3d22f63a53c8f0b9b96c553',
-        client_secret = '6b8bbb0743b912695afd94fe0187c7b7',
-        redirect_uri='http://localhost:2020/petitionresolved'
-        )
-        return soundCloudClient
+        return sound_cloud_client
